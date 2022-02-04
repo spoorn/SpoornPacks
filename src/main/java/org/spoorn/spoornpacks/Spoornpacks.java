aae204a6032cd7e664135000dfff8e5f9483d3bc
spoorn <spookump@gmail.com>
2022-02-04 01:34:00 -0800

    Initial commit

---
+++ src/main/java/org/spoorn/spoornpacks/Spoornpacks.java
@@ -0,0 +1,10 @@
+package org.spoorn.spoornpacks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Spoornpacks implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/spoorn/spoornpacks/client/SpoornpacksClient.java
@@ -0,0 +1,13 @@
+package org.spoorn.spoornpacks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SpoornpacksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
