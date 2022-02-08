b4b46d66b6fbc575edaf211d5aea3f414a649c5f
spoorn <spookump@gmail.com>
2022-02-07 19:49:41 -0800

    Fix dependency build for jackson libraries

--- src/main/java/org/spoorn/spoornpacks/SpoornPacks.java
+++ src/main/java/org/spoorn/spoornpacks/SpoornPacks.java
@@ -9,32 +9,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.log4j.Log4j2;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
 import net.minecraft.resource.ResourcePackSource;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.gen.feature.TreeConfiguredFeatures;
-import org.spoorn.spoornpacks.api.Resource;
-import org.spoorn.spoornpacks.api.ResourceBuilder;
-import org.spoorn.spoornpacks.api.ResourceFactory;
-import org.spoorn.spoornpacks.exception.DuplicateNameException;
-import org.spoorn.spoornpacks.core.generator.ResourceGenerator;
-import org.spoorn.spoornpacks.registry.SpoornPacksRegistry;
-import org.spoorn.spoornpacks.type.BlockType;
-import org.spoorn.spoornpacks.type.ItemType;
-
-import java.util.Optional;
 
 @Log4j2
 public class SpoornPacks implements ModInitializer {
 
-    public static final String MODID = "SpoornPacks";
+    public static final String MODID = "spoornpacks";
     public static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     public static DefaultPrettyPrinter PRETTY_PRINTER = new DefaultPrettyPrinter();
-    //private static ResourceGenerator RESOURCE_GENERATOR = SpoornPacksRegistry.registerResource("spoornpink");
+    //private static ResourceGenerator RESOURCE_GENERATOR = SpoornPacksRegistry.registerResource("mymodid");
 
     public static ResourcePackSource RESOURCE_PACK_SOURCE = ResourcePackSource.nameAndSource("pack.source.spoornpacks");
 
@@ -50,27 +33,27 @@ public class SpoornPacks implements ModInitializer {
     @Override
     public void onInitialize() {
         log.info("Hello from SpoornPacks!");
-
+        
         //test();
     }
 
 //    public static void test() {
 //        try {
-//            String defaultName = "pink_blossom";
+//            String defaultName = "red_blossom";
 //
 //            ItemGroup itemGroup = FabricItemGroupBuilder.build(
-//                    new Identifier("spoornpink", "general"),
-//                    ResourceFactory.fetchItemGroupSupplierFromBlock("spoornpink", "pink_blossom_sapling")
+//                    new Identifier("mymodid", "general"),
+//                    ResourceFactory.fetchItemGroupSupplierFromBlock("mymodid", "red_blossom_sapling")
 //            );
 //
-//            ResourceBuilder rb = ResourceFactory.create("spoornpink", defaultName, itemGroup)
+//            ResourceBuilder rb = ResourceFactory.create("mymodid", defaultName, itemGroup)
 //                    .addBlocks(BlockType.LOG, BlockType.WOOD, BlockType.PLANKS, BlockType.FENCE, BlockType.FENCE_GATE, 
 //                            BlockType.LEAVES, BlockType.BUTTON, BlockType.SLAB, BlockType.PRESSURE_PLATE, BlockType.STAIRS,
 //                            BlockType.TRAPDOOR, BlockType.DOOR, BlockType.CRAFTING_TABLE, BlockType.STRIPPED_LOG, BlockType.STRIPPED_WOOD)
 //                    .addItems(ItemType.LOG, ItemType.WOOD, ItemType.PLANKS, ItemType.FENCE, ItemType.FENCE_GATE, 
 //                            ItemType.LEAVES, ItemType.BUTTON, ItemType.SLAB, ItemType.PRESSURE_PLATE, ItemType.STAIRS,
 //                            ItemType.TRAPDOOR, ItemType.DOOR, ItemType.CRAFTING_TABLE, ItemType.BOAT, ItemType.STRIPPED_LOG, ItemType.STRIPPED_WOOD)
-//                    .addLeavesWithSaplingOverride("dark_pink_blossom", defaultName).addItem(ItemType.LEAVES, "dark_pink_blossom")
+//                    .addLeavesWithSaplingOverride("yellow_blossom", defaultName).addItem(ItemType.LEAVES, "yellow_blossom")
 //                    .addSapling(TreeConfiguredFeatures.OAK).addItem(ItemType.SAPLING)  // You should have your own ConfiguredFeature for trees
 //                    ;
 //
