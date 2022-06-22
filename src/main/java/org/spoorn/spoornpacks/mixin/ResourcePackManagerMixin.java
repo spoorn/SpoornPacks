4bc623c7c277e52929932e47065f1003af3bd1ca
spoorn <spookump@gmail.com>
2022-06-21 17:44:13 -0700

    Use Inject when adding the ResourcePackProvider to avoid conflicts with other Redirects
    Fixes https://github.com/spoorn/SpoornPacks/issues/5

--- src/main/java/org/spoorn/spoornpacks/mixin/ResourcePackManagerMixin.java
+++ src/main/java/org/spoorn/spoornpacks/mixin/ResourcePackManagerMixin.java
@@ -1,41 +1,39 @@
 package org.spoorn.spoornpacks.mixin;
 
-import com.google.common.collect.ImmutableSet;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.resource.ResourcePackProfile;
 import net.minecraft.resource.ResourcePackProvider;
-import org.apache.commons.lang3.ArrayUtils;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spoorn.spoornpacks.core.SPClientResourcePackProvider;
 import org.spoorn.spoornpacks.core.SPServerResourcePackProvider;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Mixin(ResourcePackManager.class)
 public class ResourcePackManagerMixin {
     
-    @Shadow @Final private Set<ResourcePackProvider> providers;
+    @Shadow @Final @Mutable private Set<ResourcePackProvider> providers;
 
     /**
      * Injects our custom resource pack provider.
      */
-    @Redirect(method = "<init>(Lnet/minecraft/resource/ResourcePackProfile$Factory;[Lnet/minecraft/resource/ResourcePackProvider;)V",
-            at = @At(value = "INVOKE", target = "Lcom/google/common/collect/ImmutableSet;copyOf([Ljava/lang/Object;)Lcom/google/common/collect/ImmutableSet;"))
-    private <E> ImmutableSet<Object> injectSPResourcePack(E[] elements) {
-        //System.out.println("### injectSPResourcePack");
+    @Inject(method = "<init>(Lnet/minecraft/resource/ResourcePackProfile$Factory;[Lnet/minecraft/resource/ResourcePackProvider;)V", at = @At(value = "RETURN"))
+    private <E> void injectSPResourcePack(ResourcePackProfile.Factory profileFactory, ResourcePackProvider[] resourcePackProviders, CallbackInfo ci) {
+        providers = new HashSet<>(providers);
+        
         boolean isClient = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
         boolean providerPresent = false;
 
-        for (int i = 0; i < elements.length; i++) {
-            E element = elements[i];
-
+        for (ResourcePackProvider element : providers) {
             if (element instanceof SPClientResourcePackProvider) {
                 isClient = true;
                 providerPresent = true;
@@ -51,14 +49,12 @@ public class ResourcePackManagerMixin {
         if (!providerPresent) {
             if (isClient) {
                 //System.out.println("### adding client");
-                return ImmutableSet.copyOf(ArrayUtils.add(elements, new SPClientResourcePackProvider()));
+                providers.add(new SPClientResourcePackProvider());
             } else {
                 //System.out.println("### adding server");
-                return ImmutableSet.copyOf(ArrayUtils.add(elements, new SPServerResourcePackProvider()));
+                providers.add(new SPServerResourcePackProvider());
             }
         }
-
-        return ImmutableSet.copyOf(elements);
     }
 
     @Inject(method = "scanPacks", at = @At(value = "TAIL"))
