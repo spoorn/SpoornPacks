14a67fdc455466e50ab6ebc2ebe0a50e305d5c21
spoorn <spookump@gmail.com>
2022-08-01 03:42:37 -0700

    Copy fabric's NamespaceResourceManagerAccessor which doesn't exist on Quilt
    Fixes
    https://discord.com/channels/990178919795785749/990183000492900403/1003409144927240252
    
    There could be more conflicts like these though

--- src/main/java/org/spoorn/spoornpacks/core/SPGroupResourcePack.java
+++ src/main/java/org/spoorn/spoornpacks/core/SPGroupResourcePack.java
@@ -3,7 +3,6 @@ package org.spoorn.spoornpacks.core;
 import lombok.extern.log4j.Log4j2;
 import net.fabricmc.fabric.impl.resource.loader.FabricNamespaceResourceManagerEntry;
 import net.fabricmc.fabric.impl.resource.loader.ModResourcePackCreator;
-import net.fabricmc.fabric.mixin.resource.loader.NamespaceResourceManagerAccessor;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.SharedConstants;
 import net.minecraft.resource.NamespaceResourceManager;
@@ -16,6 +15,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.Nullable;
+import org.spoorn.spoornpacks.mixin.NamespaceResourceManagerAccessor;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -176,7 +176,7 @@ public class SPGroupResourcePack implements ResourcePack {
             return;
         }
 
-        Identifier metadataId = NamespaceResourceManagerAccessor.fabric$accessor_getMetadataPath(id);
+        Identifier metadataId = NamespaceResourceManagerAccessor.spoorn$accessor_getMetadataPath(id);
 
         for (SPResourcePack pack : packs) {
             if (pack.contains(manager.getType(), id)) {
---
+++ src/main/java/org/spoorn/spoornpacks/mixin/NamespaceResourceManagerAccessor.java
@@ -0,0 +1,25 @@
+package org.spoorn.spoornpacks.mixin;
+
+import net.minecraft.resource.NamespaceResourceManager;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+/**
+ * Copy of {@link net.fabricmc.fabric.mixin.resource.loader.NamespaceResourceManagerAccessor}.
+ * 
+ * Copied to support Quilt.
+ */
+@Mixin(NamespaceResourceManager.class)
+public interface NamespaceResourceManagerAccessor {
+    
+    @Accessor("type")
+    ResourceType getType();
+
+    @Invoker("getMetadataPath")
+    static Identifier spoorn$accessor_getMetadataPath(Identifier id) {
+        throw new UnsupportedOperationException("Invoker injection failed.");
+    }
+}
